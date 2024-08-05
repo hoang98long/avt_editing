9f05fdc4cfa07dbeea0c19fcafacedb9846cf70c
hoang98long <53008046+hoang98long@users.noreply.github.com>
2024-08-06 00:50:00 +0700

    up

--- avt_editing.sh
+++ avt_editing.sh
@@ -2,6 +2,7 @@
 
 CONFIG_FILE=$1
 
+# shellcheck disable=SC2164
 cd /home/avt/github/avt_editing
 conda activate avt_editing
-python main.py "$CONFIG_FILE"
+python main.py --config_file "$CONFIG_FILE"
